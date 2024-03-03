0830cdc079fad87cbb2078ebee09612467d99465
Mr-Smits <stan.smits.25@gmail.com>
2024-03-03 18:02:44 +0100

    fix: time stamp au format excel + mesures toutes les 5mins

--- PiouPiou_1.0/PiouPiou_1.0.ino
+++ PiouPiou_1.0/PiouPiou_1.0.ino
@@ -7,21 +7,11 @@
 #include <ScioSense_ENS160.h>
 
 DS3231 myRTC;
-RTClib Date;
-bool h12Flag, pmFlag, century = false;
-DateTime now;
-char Date_Time_STR[21]; //eg : "25/12/2024,00:00:00," = 20 caratéres + \0
-
 AHT20 aht20;
-float Temperature;
-char Temp_STR[6];
-float Humidity;
-char Hum_STR[6];
-
 ScioSense_ENS160 ens160(ENS160_I2CADDR_1);
-uint16_t eCO2;
 
-char Sensors_Values_STR[17];  //eg : "25.00,55.00,550," = 16 caractères + \0
+char Date_Time_STR[16]; //eg : "25-12-24 00:00," = 15 caratéres + \0
+char Sensors_Values_STR[15];  //eg : "25.0,55.0,550," = 14 caractères + \0
 
 void setup() {
   Serial.begin(9600);
@@ -37,9 +27,10 @@ void setup() {
 void loop() {
   get_Date_Time();
   get_sensors_values();
-  Serial.print(Date_Time_STR);Serial.println(Sensors_Values_STR);
   save_to_SD();
-  delay(5000);
+
+  Serial.print(Date_Time_STR);Serial.println(Sensors_Values_STR);
+  delay(300000);
 }
 
 void init_Date_Time(void){
@@ -55,26 +46,31 @@ void init_Date_Time(void){
 }
 
 void get_Date_Time(void){
-  DateTime now = Date.now();
-  sprintf(Date_Time_STR,"%02d/%02d/%02d,%02d:%02d:%02d,", now.day(), now.month(), now.year(), now.hour(), now.minute(), now.second());
+  bool h12, PM_time, Century = false;
+  sprintf(Date_Time_STR,"%02d-%02d-%02d %02d:%02d,", myRTC.getDate(), myRTC.getMonth(Century), myRTC.getYear(), myRTC.getHour(h12, PM_time), myRTC.getMinute());
 }
 
 void get_sensors_values(void){
-  if(aht20.available()){
+  if(aht20.available() && ens160.available()){
+    float Temperature;
+    float Humidity;
+    uint16_t eCO2;
+
+    char Temp_STR[5];
+    char Hum_STR[5];
+
     Temperature = aht20.getTemperature();
     Humidity = aht20.getHumidity();
-  }
-  
-  if(ens160.available()){
+
     ens160.set_envdata(Temperature, Humidity);
     ens160.measure(true);
     ens160.measureRaw(true);
     eCO2 = ens160.geteCO2();
-  }
 
-  dtostrf(Temperature, 5, 2, Temp_STR);
-  dtostrf(Humidity, 5, 2, Hum_STR);
-  sprintf(Sensors_Values_STR,"%s,%s,%03d",Temp_STR, Hum_STR, eCO2);
+    dtostrf(Temperature, 4, 1, Temp_STR);
+    dtostrf(Humidity, 4, 1, Hum_STR);
+    sprintf(Sensors_Values_STR,"%s,%s,%03d",Temp_STR, Hum_STR, eCO2);
+  }
 }
 
 void save_to_SD(void){
@@ -86,9 +82,8 @@ void save_to_SD(void){
         SD_File.print(Date_Time_STR);
         SD_File.println(Sensors_Values_STR);
         SD_File.close();
-        Serial.println("Data writed to file !");
       }
-      else Serial.println("Fail opening file !");
+      else Serial.println(F("Fail opening file !"));
     }
-    else Serial.println("File doesn't exist !");
+    else Serial.println(F("File doesn't exist !"));
 }
